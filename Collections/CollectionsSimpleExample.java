344f0eec2dc90dbd50b878917582fd17a4c07fa2
Jacobson-Keith <jac11009@byui.edu>
2018-04-02 15:20:10 -0700

    Add files via upload

---
+++ Collections/CollectionsSimpleExample.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class JavaCollections {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "Hello\n" ); 
+		
+		List list = new LinkedList();
+
+list.add("Networking");
+list.add("and Being Awesome");
+list.add(0, "Cyber Security");
+
+	      System.out.println("My Name is Keith and I like: ");
+	      System.out.println(list);     
+					
+	}
+
+}
