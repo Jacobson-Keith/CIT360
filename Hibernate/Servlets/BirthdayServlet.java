e64996c2aa8e3fcd5b0e203f2788ea651a7b6c89
Jacobson-Keith <jac11009@byui.edu>
2018-04-09 23:00:59 -0700

    Update BirthdayServlet.java

--- Hibernate/Servlets/BirthdayServlet.java
+++ Hibernate/Servlets/BirthdayServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class MessageServlet extends HttpServlet {
+public class BirthdayServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		//set response content type
